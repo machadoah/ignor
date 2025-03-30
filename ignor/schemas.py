376f20f3af40ea534c9029b287188bd79101b832
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-03-30 17:26:00 -0300

    ✨ Add Pydantic models for templates and template list in schemas.py

--- ignor/schemas.py
+++ ignor/schemas.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+
+class Templates(BaseModel):
+    templates: list[str]
+
+
+class Template(BaseModel):
+    template: str
